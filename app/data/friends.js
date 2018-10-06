a5a162acbda69427879ba330496a72951ee3ada7
Ilya <ilyalutskiy@gmail.com>
2018-10-06 14:59:31 -0500

    mostly working code

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Bojack",
+		photo: "https://vignette.wikia.nocookie.net/bojackhorseman/images/d/d2/BoJack_Horsemann.png",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Dianne",
+		photo: "https://vignette.wikia.nocookie.net/bojackhorseman/images/6/60/Diane_Nguyen_Season_5_haircut.png",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Princess Carolyn",
+		photo: "https://vignette.wikia.nocookie.net/bojackhorseman/images/d/de/Princess_Carolyn.png",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Mr Peanutbutter",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Todd",
+		photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/f/fe/Season_7_-_Frank.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
